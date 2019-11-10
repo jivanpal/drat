5764e558389696cce72540319a213463529f6367
Jivan Pal <jivan.pal@gmail.com>
2019-11-10 23:26:20 +0000

    Updated `apfs-read`; now displays more details for the types of blocks we have implemented `print` routines for

--- src/apfs-read.c
+++ src/apfs-read.c
@@ -6,7 +6,15 @@
 
 #include "apfs/io.h"
 #include "apfs/struct/general.h"
+
+#include "apfs/func/boolean.h"
+#include "apfs/func/cksum.h"
+
 #include "apfs/string/object.h"
+#include "apfs/string/nx.h"
+#include "apfs/string/btree.h"
+#include "apfs/string/omap.h"
+#include "apfs/string/fs.h"
 
 /**
  * Print usage info for this program.
@@ -47,17 +55,49 @@ int main(int argc, char** argv) {
     }
     printf("OK.\n\n");
 
-    char* block_buf = malloc(nx_block_size);
-    if (read_blocks(block_buf, nx_block_addr, 1) == 0) {
-        printf("END: Block index %s does not exist in `%s`.\n", argv[2], nx_path);
+    printf("Reading block 0x%llx ... ", nx_block_addr);
+    obj_phys_t* block = malloc(nx_block_size);
+    if (read_blocks(block, nx_block_addr, 1) == 0) {
+        printf("\nEND: Block index %s does not exist in `%s`.\n", argv[2], nx_path);
         goto cleanup;
     }
-    printf("- Details of block 0x%llx:\n", nx_block_addr);
-    print_obj_phys(block_buf);
+    printf("validating ... ");
+    if (is_cksum_valid(block)) {
+        printf("OK.\n");
+    } else {
+        printf("FAILED.\nThe specified block may contain file-system data or be free space.\n");
+    }
+    printf("\n");
+    
+    printf("Details of block 0x%llx:\n", nx_block_addr);
+    printf("--------------------------------------------------------------------------------\n");
+    switch (block->o_type & OBJECT_TYPE_MASK) {
+        case OBJECT_TYPE_NX_SUPERBLOCK:
+            print_nx_superblock(block);
+            break;
+        case OBJECT_TYPE_BTREE:
+        case OBJECT_TYPE_BTREE_NODE:
+            print_btree_node_phys(block);
+            break;
+        case OBJECT_TYPE_OMAP:
+            print_omap_phys(block);
+            break;
+        case OBJECT_TYPE_CHECKPOINT_MAP:
+            print_checkpoint_map_phys(block);
+            break;
+        case OBJECT_TYPE_FS:
+            print_apfs_superblock(block);
+            break;
+        default:
+            print_obj_phys(block);
+            printf("\n--- This tool cannot currently display more details about this type of block ---\n");
+            break;
+    }
+    printf("--------------------------------------------------------------------------------\n");
     printf("\nEND: All done.\n");
 
 cleanup:
-    free(block_buf);
+    free(block);
     fclose(nx);
     return 0;
 }
