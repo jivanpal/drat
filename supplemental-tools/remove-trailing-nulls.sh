cecc18d0c5eabe0a89768cdc86cfc9f7a6f78b6f
Jivan Pal <jivan.pal@gmail.com>
2020-03-21 19:31:20 +0000

    Modified RTN script to specify target directory

--- supplemental-tools/remove-trailing-nulls.sh
+++ supplemental-tools/remove-trailing-nulls.sh
@@ -2,7 +2,12 @@
 
 IFS=$'\n'
 
-for f in $(find ./RECOVERY -type f); do
+if [ $# -ne 1 ] ; then
+  2>&1 echo "Usage: $0 <target directory>"
+  exit 1
+fi
+
+for f in $(find "$1" -type f); do
   echo -n "- \`$f\` -- "
   ./rtn "$f"
 done
