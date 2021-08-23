dae6f2dbab89232c3a3fa135936d62e88178302d
Jivan Pal <jivan.pal@gmail.com>
2021-08-24 00:24:34 +0100

    Simplify get_apfs_readonly_compatible_features_string()

--- include/drat/string/fs.c
+++ include/drat/string/fs.c
@@ -83,59 +83,45 @@ char* get_apfs_features_string(apfs_superblock_t* apsb) {
  *      this pointer when it is no longer needed.
  */
 char* get_apfs_readonly_compatible_features_string(apfs_superblock_t* apsb) {
-    // String to use if no flags are set
-    char* default_string = "- No read-only compatible volume feature flags are set.\n";
-    size_t default_string_len = strlen(default_string);
-    
-    const int NUM_FLAGS = 0;
-    uint64_t flag_constants[] = {
-        // empty
-    };
-    char* flag_strings[] = {
+    struct u64_string_mapping flags[] = {
         // empty
     };
 
-    // Allocate sufficient memory for the result string
-    size_t max_mem_required = 0;
-    for (int i = 0; i < NUM_FLAGS; i++) {
-        max_mem_required += strlen(flag_strings[i]) + 3;
-        // `+ 3` accounts for prepending "- " and appending "\n" to each string
-    }
-    if (max_mem_required < default_string_len) {
-        max_mem_required = default_string_len;
-    }
-    max_mem_required++; // Make room for terminating NULL byte
-
-    char* result_string = malloc(max_mem_required);
+    // Initialise buffer as empty string
+    const size_t bufsize = 2048;
+    char* result_string = malloc(bufsize);
     if (!result_string) {
-        fprintf(stderr, "\nABORT: get_apfs_readonly_compatible_features_string: Could not allocate sufficient memory for `result_string`.\n");
-        exit(-1);
+        fprintf(stderr, "\nERROR: %s: Couldn't create buffer `result_string`.\n", __func__);
+        return NULL;
     }
+    *result_string = '\0';
 
-    char* cursor = result_string;
+    size_t bytes_written = 0;
+    for (size_t i = 0; i < ARRAY_SIZE(flags); i++) {
+        if (apsb->apfs_readonly_compatible_features & flags[i].value) {
+            bytes_written += snprintf(
+                result_string + bytes_written,
+                bufsize - bytes_written,
+                "- %s\n",
+                flags[i].string
+            );
 
-    // Go through possible flags, adding corresponding string to result if
-    // that flag is set.
-    for (int i = 0; i < NUM_FLAGS; i++) {
-        if (apsb->apfs_readonly_compatible_features & flag_constants[i]) {
-            *cursor++ = '-';
-            *cursor++ = ' ';
-            memcpy(cursor, flag_strings[i], strlen(flag_strings[i]));
-            cursor += strlen(flag_strings[i]);
-            *cursor++ = '\n';
+            if (bytes_written > bufsize - 1) {
+                // Exhausted buffer; return early.
+                fprintf(stderr, "\nERROR: %s: Buffer `result_string` too small for entire result.\n", __func__);
+                return result_string;
+            }
         }
     }
 
-    if (cursor == result_string) {
-        // No strings were added, so it must be that no flags are set.
-        memcpy(cursor, default_string, default_string_len);
-        cursor += default_string_len;
+    if (bytes_written == 0) {
+        // No flags set; use default string.
+        snprintf(result_string, bufsize, "- No read-only compatible volume feature flags are set.\n");
     }
 
-    *cursor = '\0';
-
-    // Free up excess allocated memory.
+    // Truncate buffer
     result_string = realloc(result_string, strlen(result_string) + 1);
+    
     return result_string;
 }
 
