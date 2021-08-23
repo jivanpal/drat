af44689efc442cd53880dfadb555c022b6830452
Jivan Pal <jivan.pal@gmail.com>
2021-08-23 01:58:32 +0100

    Simplify get_bt_info_flags_string()

--- include/drat/string/btree.c
+++ include/drat/string/btree.c
@@ -87,75 +87,53 @@ char* get_btn_flags_string(btree_node_phys_t* btn) {
  *      this pointer when it is no longer needed.
  */
 char* get_bt_info_flags_string(btree_info_t* bt_info) {
-    char* default_string = "  - No flags are set\n";
-    size_t default_string_len = strlen(default_string);
-    
-    uint8_t NUM_FLAGS = 9;
-    
-    uint16_t flag_constants[] = {
-        BTREE_UINT64_KEYS,
-        BTREE_SEQUENTIAL_INSERT,
-        BTREE_ALLOW_GHOSTS,
-        BTREE_EPHEMERAL,
-        BTREE_PHYSICAL,
-        BTREE_NONPERSISTENT,
-        BTREE_KV_NONALIGNED,
-        BTREE_HASHED,
-        BTREE_NOHEADER,
+    struct u64_string_mapping flags[] = {
+        { BTREE_UINT64_KEYS,        "Keys are 64-bit values --- optimisations operations if possible" },
+        { BTREE_SEQUENTIAL_INSERT,  "This B-tree is currently undergoing a series of sequential inserts --- optimise operations if possible" },
+        { BTREE_ALLOW_GHOSTS,       "Ghosts (keys without values) are allowed" },
+        { BTREE_EPHEMERAL,          "Child nodes are referred to using Ephemeral OIDs" },
+        { BTREE_PHYSICAL,           "Child nodes are referred to using Physical OIDs" },
+        { BTREE_NONPERSISTENT,      "This B-tree does not persist across unmounts" },
+        { BTREE_KV_NONALIGNED,      "8-byte alignment of keys and values is not required" },
+        { BTREE_HASHED,             "Non-leaf nodes store a hash of their child nodes" },
+        { BTREE_NOHEADER,           "Nodes don't have object headers" },
     };
 
-    char* flag_strings[] = {
-        "Keys are 64-bit values --- optimisations operations if possible",
-        "This B-tree is currently undergoing a series of sequential inserts --- optimise operations if possible",
-        "Ghosts (keys without values) are allowed",
-        "Child nodes are referred to using Ephemeral OIDs",
-        "Child nodes are referred to using Physical OIDs",
-        "This B-tree does not persist across unmounts",
-        "8-byte alignment of keys and values is not required",
-        "Non-leaf nodes store a hash of their child nodes",
-        "Nodes don't have object headers",
-    };
-
-    size_t max_mem_required = 0;
-    for (uint8_t i = 0; i < NUM_FLAGS; i++) {
-        max_mem_required += strlen(flag_strings[i]) + 5;
-        // `+ 5` accounts for prepending "  - " and appending "\n" to each string
-    }
-    if (max_mem_required < default_string_len) {
-        max_mem_required = default_string_len;
-    }
-    max_mem_required++; // Account for terminating NULL byte
-
-    char* result_string = malloc(max_mem_required);
+    // Initialise buffer as empty string
+    const size_t bufsize = 2048;
+    char* result_string = malloc(bufsize);
     if (!result_string) {
-        fprintf(stderr, "\nABORT: get_bt_info_flags_string: Could not allocate sufficient memory for `result_string`.\n");
-        exit(-1);
+        fprintf(stderr, "\nERROR: %s: Couldn't create buffer `result_string`.\n", __func__);
+        return NULL;
     }
+    *result_string = '\0';
 
-    char* cursor = result_string;
-    for (uint8_t i = 0; i < NUM_FLAGS; i++) {
-        if (bt_info->bt_fixed.bt_flags & flag_constants[i]) {
-            memcpy(cursor, "  - ", 4);
-            cursor += 4;
-
-            size_t flag_string_len = strlen(flag_strings[i]);
-            memcpy(cursor, flag_strings[i], flag_string_len);
-            cursor += flag_string_len;
+    size_t bytes_written = 0;
+    for (size_t i = 0; i < ARRAY_SIZE(flags); i++) {
+        if (bt_info->bt_fixed.bt_flags & flags[i].value) {
+            bytes_written += snprintf(
+                result_string + bytes_written,
+                bufsize - bytes_written,
+                "  - %s\n",
+                flags[i].string
+            );
             
-            *cursor++  = '\n';
+            if (bytes_written > bufsize - 1) {
+                // Exhausted buffer; return early.
+                fprintf(stderr, "\nERROR: %s: Buffer `result_string` too small for entire result.\n", __func__);
+                return result_string;
+            }
         }
     }
 
-    // If no flags set, nothing is added to string, so use deafult string
-    if (cursor == result_string) {
-        memcpy(cursor, default_string, default_string_len);
-        cursor += default_string_len;
+    if (bytes_written == 0) {
+        // No flags set; use default string.
+        snprintf(result_string, bufsize, "  - No flags are set.\n");
     }
 
-    *cursor = '\0';
-
-    // De-allocate excess memory
+    // Truncate buffer
     result_string = realloc(result_string, strlen(result_string) + 1);
+
     return result_string;
 }
 
